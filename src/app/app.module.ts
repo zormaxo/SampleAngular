1bc1c2f6852a50697ccc0272961905d40d060274
zormax <zormax052@gmail.com>
2022-06-21 21:27:00 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PrivacyComponent } from './home/privacy/privacy.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/privacy/privacy.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-privacy',
+  templateUrl: './privacy.component.html',
+  styleUrls: ['./privacy.component.css']
+})
+export class PrivacyComponent implements OnInit {
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    console.log("routes");
+    console.log(this.activatedRoute.snapshot.url); // array of states
+    console.log(this.activatedRoute.snapshot.url[0].path);
+    console.log("omer");
+    console.log(this.activatedRoute.snapshot.url[this.activatedRoute.snapshot.url.length - 1].path);
+    console.log(this.activatedRoute.snapshot.url.pop()?.path);
+  }
+
+}
