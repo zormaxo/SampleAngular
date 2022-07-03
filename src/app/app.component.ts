aa4f3751ef9d982ad30d50de0436dda5d09c83a1
zormax <zormax052@gmail.com>
2022-07-03 14:24:47 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShopService } from './shop/shop.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SampleAngular';
+  deneme;
+
+  constructor(private shopService: ShopService) {
+    
+    this.deneme = shopService.omer;
+  }
 }
