b80e1714da727b95f692d58d75790aeeeefaf976
zormax <zormax052@gmail.com>
2022-07-10 07:57:12 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,12 @@ import { HomeComponent } from './home/home.component';
 import { PrivacyComponent } from './home/privacy/privacy.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PrivacyComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, HomeComponent, PrivacyComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ShopModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ShopComponent implements OnInit {
 
